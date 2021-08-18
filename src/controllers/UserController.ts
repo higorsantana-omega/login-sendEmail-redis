c7945a38040191ae62f0ff27c350911fde532258
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-08-18 15:36:33 -0300

    feat: create sendEmail

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import passwordGenerator from 'password-generator'
 
+import { nodeMailer } from "../lib/Email";
+
 class UserController {
   async store(req: Request, res: Response): Promise<void> {
     const { name, email } = req.body
@@ -10,6 +12,13 @@ class UserController {
       email,
       password: passwordGenerator(10, false)
     }
+
+    await nodeMailer.sendMail({
+      from: 'Higor <higorcontato@teste.com.br>',
+      to: `${name} ${email}`,
+      subject: 'Cadastro de usuário',
+      html: `Olá, ${name}, bem-vindo ao meu teste`
+    })
 
     res.status(201).json(user)
   }
